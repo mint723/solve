c42b9e8b863d3fc3fb3c2f49b6a1166823145523
mint723 <114153768+mint723@users.noreply.github.com>
2023-05-09 00:50:52 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.02 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,20 +1,11 @@
 class Solution {
     public int solution(int a, int b, int c) {
-        int answer = 0;
-        if (a==b && b==c) {
+        int answer = a+b+c;
+		if (a==b && b==c) {
 			answer = (a+a+a)*((a*a)+(a*a)+(a*a))*((a*a*a)+(a*a*a)+(a*a*a));
 		}
-		else if(a!=b && b==c) {
+		else if((a!=b && b==c)||(b!=a && a==c)||(c!=b && a==b)) {
 			answer = (a+b+c)*((a*a)+(b*b)+(c*c));
-		}
-		else if(b!=a && a==c) {
-			answer = (a+b+c)*((b*b)+(a*a)+(c*c));
-		}
-		else if(c!=b && a==b) {
-			answer = (a+b+c)*((c*c)+(a*a)+(b*b));
-		}
-		else {
-			answer = a+b+c;
 		}
         return answer;
     }
