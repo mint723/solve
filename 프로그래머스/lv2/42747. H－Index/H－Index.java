6a01a63efac14fe0f8799eccb72fc2facc010d1c
mint723 <114153768+mint723@users.noreply.github.com>
2023-05-09 17:29:11 +0900

    (File) [level 2] Title: H-Index, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int[] citations) {
         int answer = 1;
         int h=0;
-        
+     
         for (int i = 0; i < citations.length; i++) {
 			answer=0;
 			for (int j = 0; j < citations.length; j++) {
