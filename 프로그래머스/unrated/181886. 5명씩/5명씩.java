291510cd4530b9d981ca696b0b43a02a8743f519
mint723 <114153768+mint723@users.noreply.github.com>
2023-05-21 13:55:53 +0900

    [unrated] Title: 5명씩, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] names) {
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 0; i < names.length; i+=5) {
+            result.add(names[i]);
+        }
+        
+        String[] answer = new String[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        }
+        return answer;
+    }
+}
