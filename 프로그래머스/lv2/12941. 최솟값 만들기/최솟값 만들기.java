58c556331159392e26ed13389309dd63f4846da8
mint723 <114153768+mint723@users.noreply.github.com>
2023-06-09 16:04:18 +0900

    [level 2] Title: 최솟값 만들기, Time: 5.72 ms, Memory: 52.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,22 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        PriorityQueue<Integer> a = new PriorityQueue<>();
+        PriorityQueue<Integer> b = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int i = 0; i < A.length; i++) {
+            a.add(A[i]);
+            b.add(B[i]);
+        }
+
+        while(!a.isEmpty()){
+            answer+=a.poll()*b.poll();
+        }
+
+        return answer;
+    }
+}
