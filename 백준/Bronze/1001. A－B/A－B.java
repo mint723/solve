673a53015820e77b6938f16958bb7c90124e207a
mint723 <114153768+mint723@users.noreply.github.com>
2024-03-25 20:28:45 +0900

    [Bronze V] Title: A-B, Time: 140 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String brReadLine = br.readLine();
+        String[] s = brReadLine.split(" ");
+        System.out.println(Integer.parseInt(s[0])-Integer.parseInt(s[1]));
+        br.close();
+    }
+}
