db8c3b7005bf98efdd180c24500942b6299219f1
mint723 <114153768+mint723@users.noreply.github.com>
2024-07-01 11:25:20 +0900

    [level 1] Title: 최소직사각형, Time: 1.04 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] sizes) {
+        return calc(sizes);
+    }
+    
+    static int calc(int[][] sizes){
+        int widthMax = 0;
+        int heightMax = 0;
+        for(int i=0; i<sizes.length; i++){
+            if(sizes[i][0]<sizes[i][1]){
+                int tmp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = tmp;
+            }
+            if(sizes[i][0]>widthMax) widthMax = sizes[i][0];
+            if(sizes[i][1]>heightMax) heightMax = sizes[i][1];
+        }
+        return widthMax*heightMax;
+    }
+}
