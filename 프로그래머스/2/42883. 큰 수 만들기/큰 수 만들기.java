74f62a65f8d058f25d6f4b4caa5c67a3e86ecb2c
mint723 <114153768+mint723@users.noreply.github.com>
2024-10-11 11:18:54 +0900

    [level 2] Title: 큰 수 만들기, Time: 103.45 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public String solution(String number, int k) {
+        Stack<Integer> stack = new Stack<>();
+        int count = 0;
+        for(int i=0; i<number.length(); i++){
+            int num = number.charAt(i)-'0';
+            while(!stack.isEmpty() && stack.peek() < num && count<k){
+                stack.pop();
+                count++;
+            }
+            stack.push(num);
+        }
+        
+        if(k != count){
+            while(k!=count){
+                stack.pop();
+                count++;
+            }
+        }
+        
+        return stack.stream().map(String::valueOf).collect(Collectors.joining(""));
+    }
+}
