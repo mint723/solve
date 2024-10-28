3c0def9651152ea8c0429b7bb389e7d7ccdd6d1e
mint723 <114153768+mint723@users.noreply.github.com>
2024-10-28 16:06:52 +0900

    [level 2] Title: 오픈채팅방, Time: 288.62 ms, Memory: 196 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -3,7 +3,6 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] record) {
         List<String> list = new ArrayList<>();
-        Stack<String> stack = new Stack<>();
         Map<String, String> map = new HashMap<>();
         
         for(int i=0; i<record.length; i++){
@@ -16,25 +15,18 @@ class Solution {
         //마지막으로 변경된 닉네임, 혹은 닉네임으로 answer 완성하기
         for(int i=0; i<record.length; i++){
             StringBuilder sb = new StringBuilder();
-            String tmp = record[i];
-            String[] sliceTmp = record[i].split(" ");
-            String behavior = sliceTmp[0];
+            String[] name = record[i].split(" ");
+            String behavior = name[0];
             if(behavior.equals("Enter")){
-                sb.append(map.get(sliceTmp[1]));
+                sb.append(map.get(name[1]));
                 sb.append("님이 들어왔습니다.");
                 list.add(sb.toString());
             }else if(behavior.equals("Leave")){
-                sb.append(map.get(sliceTmp[1]));
+                sb.append(map.get(name[1]));
                 sb.append("님이 나갔습니다.");
                 list.add(sb.toString());
             }
         }
-        
-        String[] answer = new String[list.size()];
-        for(int i=0; i<list.size(); i++){
-            answer[i] = list.get(i);
-        }
-        
-        return answer;
+        return list.stream().toArray(String[]::new);
     }
 }
