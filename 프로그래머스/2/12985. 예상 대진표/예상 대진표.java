696e620468aae719c84d5b4836baf65b2c164be1
mint723 <114153768+mint723@users.noreply.github.com>
2024-11-03 17:33:29 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -4,16 +4,8 @@ class Solution
     {
         int answer = 0;
         while(A!=B){
-            if(A%2==0){
-                A=A/2;
-            }else{
-                A=A/2+1;
-            }
-            if(B%2==0){
-                B=B/2;
-            }else{
-                B=B/2+1;
-            }
+            A = (A+1)/2;
+            B = (B+1)/2;
             answer++;
         }
         return answer;
