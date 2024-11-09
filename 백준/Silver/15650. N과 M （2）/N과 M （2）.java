e387128ab6eb556a2b7db7dc92a9f6025649b5ff
mint723 <114153768+mint723@users.noreply.github.com>
2024-11-09 16:23:17 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static int N,M;
+    static StringBuilder sb = new StringBuilder();
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N+1];
+
+        dfs(1,0,new int[M+1]);
+        System.out.println(sb);
+    }
+
+    static void dfs(int n, int depth, int[] arr){
+        if(depth == M){
+            for (int i = 0; i < arr.length-1; i++) {
+                sb.append(arr[i]);
+                sb.append(" ");
+            }
+            sb.append("\n");
+        }else{
+            for (int i = n; i <= N; i++) {
+                arr[depth] = i;
+                dfs(i+1, depth+1,arr);
+            }
+        }
+    }
+}
