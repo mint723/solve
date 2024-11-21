07a8d8a15d31d7216be79a41c21b77e2e471ed9c
mint723 <114153768+mint723@users.noreply.github.com>
2024-11-21 15:50:04 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 148 ms, Memory: 23192 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        boolean[] visited = new boolean[100001];
+
+        Queue<int[]> q = new LinkedList<>();
+
+        visited[N] = true;
+        q.offer(new int[]{N,0});
+
+        while(!q.isEmpty()){
+            int[] arr = q.poll();
+            int point = arr[0];
+            int count = arr[1];
+            if(point == K){
+                System.out.println(count);
+                break;
+            }
+
+            int[] nextPoints = {point*2, point-1, point+1};
+
+            for(int i=0; i<3; i++){
+                int nextPoint = nextPoints[i];
+                if(0<=nextPoint && nextPoint <= 100000 && !visited[nextPoint]){
+                    visited[nextPoint] = true;
+                    q.offer(new int[]{nextPoint, i==0 ? count : count+1});
+                }
+            }
+        }
+        br.close();
+    }
+}
