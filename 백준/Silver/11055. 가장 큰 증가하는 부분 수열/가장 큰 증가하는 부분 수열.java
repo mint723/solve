39c86b4c4db6ce3e7548fa1f72b955a7e07932e4
mint723 <114153768+mint723@users.noreply.github.com>
2025-01-02 21:01:22 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 128 ms, Memory: 15116 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N+1];
+        int[] sums = new int[N+1];
+        int[] dp = new int[N+1];
+
+        for(int i=1; i<=N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+            sums[i] = arr[i];
+        }
+
+        Arrays.fill(dp,1);
+
+        for(int i=2; i<=N; i++){
+            for (int j = 1; j < i; j++) {
+                if(arr[i] > arr[j] && dp[i] <= dp[j]){
+                    dp[i] = dp[j] + 1;
+                    sums[i] = Math.max(sums[i], sums[j]+arr[i]);
+                }
+            }
+        }
+
+        System.out.println(Arrays.stream(sums).max().getAsInt());
+        br.close();
+    }
+}
