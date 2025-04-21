02c3401f6f3abf629c6d17077452de06fd89272c
mint723 <114153768+mint723@users.noreply.github.com>
2025-04-22 00:00:35 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        for(int i=1; i<=n; i=i+2){
+            list.add(i);
+        }
+        return list;
+    }
+}
