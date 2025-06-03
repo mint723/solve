28e6c6804dbfc0b14db8e694100f3204b0133c86
mint723 <114153768+mint723@users.noreply.github.com>
2025-06-04 04:09:22 +0900

    [level 2] Title: 중복 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
+++ 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
@@ -1,3 +1,6 @@
 -- 코드를 입력하세요
-SELECT count(*) as count
-FROM (SELECT * FROM animal_ins GROUP BY name HAVING name is not null) as gro_name;
+SELECT count(*)
+FROM (
+SELECT * FROM animal_ins GROUP BY name HAVING name IS NOT NULL
+) as name_table
+;
