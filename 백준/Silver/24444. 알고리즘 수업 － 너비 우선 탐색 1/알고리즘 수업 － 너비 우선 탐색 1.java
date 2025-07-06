8b146d69fde477ff785c9ccf43ecda0919b64be8
mint723 <114153768+mint723@users.noreply.github.com>
2025-07-06 15:33:35 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 932 ms, Memory: 90240 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -1,59 +1,57 @@
 import java.util.*;
 import java.io.*;
 
-class Main{
-    static int N,M;
-
-    public static void main(String[] args) throws IOException{
+class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
 
-        int startPoint = Integer.parseInt(st.nextToken());
-
-        int count = 1;
-
-        List<Integer>[] list = new List[N+1];
+        List<Integer>[] adjList = new List[N+1];
         boolean[] visited = new boolean[N+1];
         int[] answer = new int[N+1];
+        int count = 1;
 
         for(int i=0; i<=N; i++){
-            list[i] = new ArrayList<>();
+            adjList[i] = new ArrayList<>();
         }
 
         for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine());
-            int point1 = Integer.parseInt(st.nextToken());
-            int point2 = Integer.parseInt(st.nextToken());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
 
-            list[point1].add(point2);
-            list[point2].add(point1);
+            adjList[from].add(to);
+            adjList[to].add(from);
         }
 
-        for(int i=0; i<N; i++){
-            Collections.sort(list[i]);
+        for(List<Integer> list : adjList){
+            list.sort((o1,o2) -> o1 - o2);
         }
-        Queue<Integer> q = new LinkedList<>();
 
-        visited[startPoint] = true;
-        q.offer(startPoint);
-        answer[startPoint] = count++;
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(R);
+        visited[R] = true;
+        answer[R] = count++;
 
         while(!q.isEmpty()){
-            int point = q.poll();
-            for(int n : list[point]){
-                if(!visited[n]){
-                    visited[n] = true;
-                    answer[n] = count++;
-                    q.offer(n);
+            int curNode = q.poll();
+
+            for(int nextNode : adjList[curNode]){
+                if(!visited[nextNode]) {
+                    q.offer(nextNode);
+                    visited[nextNode] = true;
+                    answer[nextNode] = count++;
                 }
             }
         }
 
-        for (int i = 1; i <= N; i++) {
-            sb.append(answer[i]).append("\n");
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=1; i<=N; i++){
+            sb.append(answer[i]).append('\n');
         }
 
         System.out.println(sb);
